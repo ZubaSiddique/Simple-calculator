55851d0966757a6257c2fd7482692158f9f952e5
mini coder <zubamughal5678@gmail.com>
2025-12-05 19:10:19 +0500

    Done

--- calculator.js
+++ calculator.js
@@ -1,45 +1,53 @@
-let firstnumber="";
-let operator="";
-let secondnumber="";
-function updateDisplay(value){
-    document.getElementById("display").innerText=value;
+let firstnumber = "";
+let operator = "";
+let secondnumber = "";
+
+function updateFullDisplay() {
+    let full = firstnumber;
+    if (operator !== "") full += " " + operator + " ";
+    if (secondnumber !== "") full += secondnumber;
+
+    document.getElementById("display").innerText = full || "0";
 }
-function press(num){
-    if(operator===""){
-        firstnumber +=num;
-        updateDisplay(firstnumber);
-    }else{
-        secondnumber +=num;
-        updateDisplay(secondnumber);
+
+function press(num) {
+    if (operator === "") {
+        firstnumber += num;
+    } else {
+        secondnumber += num;
     }
+    updateFullDisplay();
 }
-function operation(op){
-    if(firstnumber!==""){
+
+function operation(op) {
+    if (firstnumber !== "") {
         operator = op;
+        updateFullDisplay();
     }
 }
-function calculate(){
-    if( firstnumber !=="" && operator!=="" && secondnumber !==""){
-        let a=Number(firstnumber);
-        let b=Number(secondnumber);
-        let result=0;
 
-        if (operator==="+")result=a+b;
-       else if (operator==="-")result=a-b;
-       else if (operator==="*")result=a*b;
-        else if (operator==="/")result=a/b;
-updateDisplay(result);
+function calculate() {
+    if (firstnumber !== "" && operator !== "" && secondnumber !== "") {
+        let a = Number(firstnumber);
+        let b = Number(secondnumber);
+        let result = 0;
 
-firstnumber=String(result);
-operator="";
-secondnumber="";
+        if (operator === "+") result = a + b;
+        else if (operator === "-") result = a - b;
+        else if (operator === "*") result = a * b;
+        else if (operator === "/") result = a / b;
 
+        firstnumber = String(result);
+        operator = "";
+        secondnumber = "";
+
+        updateFullDisplay(); // show result
     }
 }
-function clearall(){
-    firstnumber="";
-    operator="";
-    secondnumber="";
-    updateDisplay("0");
 
+function clearall() {
+    firstnumber = "";
+    operator = "";
+    secondnumber = "";
+    updateFullDisplay();
 }
